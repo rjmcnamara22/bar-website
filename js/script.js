9cef43be94955f1820f3c4f71f518a35a85f9655
Ryan_McNamara <rjmcnamara22@gmail.com>
2026-04-24 18:25:38 -0400

    Attempted fix for photo gallery appearance on mobile

--- js/script.js
+++ js/script.js
@@ -1,30 +1,31 @@
-
 let scrollContainer = document.querySelector(".gallery");
 let backBtn = document.getElementById("backBtn");
 let nextBtn = document.getElementById("nextBtn");
 
+const getScrollAmount = () => scrollContainer.clientWidth;
+
 scrollContainer.addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY;
-    scrollContainer.style.scrollBehavior = "auto";
+  evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltaY;
+  scrollContainer.style.scrollBehavior = "auto";
 });
 
 nextBtn.addEventListener("click", () => {
-    scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft += 1500;
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += getScrollAmount();
 });
 
 backBtn.addEventListener("click", () => {
-    scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft -= 1500;
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= getScrollAmount();
 });
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
